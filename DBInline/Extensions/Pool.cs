362de88d622cba89583a2b5d9499c33619612329
NicoZweifel <34443492+NicoZweifel@users.noreply.github.com>
2020-02-09 02:34:00 +0100

    feature/Readme.md (#4)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Feature/readme.md (#3)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * removed overload

--- DBInline/Extensions/Pool.cs
+++ DBInline/Extensions/Pool.cs
@@ -151,13 +151,5 @@ namespace DBInline
         {
             return PoolAsync(null, body);
         }
-        /// <summary>
-        /// Groups Transactions.
-        /// </summary>
-        /// <param name="body">Execution body."></param>
-        public static  Task<T> PoolAsync<T>(Func<IPool, Task<T>> body)
-        {
-            return PoolAsync(null, body);
-        }
     }
 }
